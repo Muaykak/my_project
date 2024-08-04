888241bfab833f837ebcd9e2c5e4720ecc7ed27e
Suttawit <sutawith001@gmail.com>
2024-08-04 19:08:05 +0700

    .

---
+++ password_library/main.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_putstr(char *str);
+void	ft_putnbr(int nbr);
+
+int main()
+{
+    int s = 13;
+    int x;
+    while (s--) {
+        x = getchar();
+        putchar(x);
+    }
+    return 0;
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nbr)
+{
+	char	c;
+
+	if (nbr < -2147483647)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nbr < 0)
+	{
+		nbr = nbr * -1;
+		write(1, "-", 1);
+	}
+	if (nbr / 10 != 0)
+	{
+		ft_putnbr(nbr / 10);
+	}
+	c = (nbr % 10) + 48;
+	write(1, &c, 1);
+}
