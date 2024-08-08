a73f1b808b358ea9fcb3ece46fd0a6df75bf4989
Muaykak <sutawith001@gmail.com>
2024-08-08 01:25:59 +0000

    .

--- password_library/main.c
+++ password_library/main.c
@@ -8,28 +8,68 @@
 void	ft_putstr(char *str);
 void	ft_putnbr(int nbr);
 int		line_strcount(int line_num);
-char	*get_line(int line_num);
+char	*malloc_get_line(int line_num);
 int		open_line(int line_num);
 int		check_printable(char *str);
 int		check_argv_printable(int ac, char **av);
 int		check_input_format(char *str);
 int		check_all_input_format(int ac, char **av);
+int		ft_strlen(char *str);
+char	*malloc_dup_lowercase(char *str);
 
 int	main(int ac, char **av)
 {
+	char	*dup;
+
 	if (check_argv_printable(ac, av) == 0)
 	{
 		ft_putstr("The arguments should have only printable characters");
 		return (0);
 	}
-	if (ac > 3)
+	if (ac == 2)
 	{
-		check_all_input_format(ac, av);
-		return (0);
+		dup = malloc_dup_lowercase(av[1]);
+		ft_putstr(dup);
+		ft_putstr("\n");
+		free(dup);
 	}
 
 }
 
+//chage the given string into lowercase and put it into malloc()
+// ******* NEED   free() ***********
+char	*malloc_dup_lowercase(char *str)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	temp = (char *)malloc((ft_strlen(str) + 1) * sizeof(char));
+	if (temp == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			temp[i] = str[i] + 32;
+		else
+			temp[i] = str[i];
+		i++;
+	}
+	temp[i] = '\0';
+	return (&temp[0]);
+}
+//count how many characters in the given string
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 // to check when having more than 3 arguments
 // to formar shoult be "Name: something describe"
 // this function is to determine ":" and " " is there in the arguments
@@ -41,7 +81,8 @@ int	check_all_input_format(int ac, char **av)
 		ft_putstr("The argument count is less than 4 thus should not come to check_input_format() function.\n");
 		return (0);
 	}
-	i = 3;
+	i = 1;
+
 	while (i < ac)
 	{
 		if (check_input_format(av[i]) == 0)
@@ -157,7 +198,8 @@ int	open_line(int line_num)
 }
 
 // this  function is to malloc the str to get to 
-char	*get_line(int line_num)
+// ******* NEED   free() ***********
+char	*malloc_get_line(int line_num)
 {
 	int		k;
 	int		fd;
@@ -168,6 +210,8 @@ char	*get_line(int line_num)
 	if (k == -1)
 		return (NULL);
 	str = (char *)malloc((k + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
 	fd = open_line(line_num);
 	i = 0;
 	while (read(fd, &str[i], 1) != 0 && str[i] != '\n')
