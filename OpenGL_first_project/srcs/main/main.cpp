78f476f3205f50cbb39b887197916146ee9bd967
srussame <sutawith001@gmail.com>
2025-10-31 08:46:35 +0700

    .

--- OpenGL_first_project/srcs/main/main.cpp
+++ OpenGL_first_project/srcs/main/main.cpp
@@ -69,9 +69,9 @@ int main()
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
+     }
     // fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
     // check for shader compile errors
