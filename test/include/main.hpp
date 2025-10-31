639d1b2b83b54141ebe2599e57dec5e4a4852de6
srussame <sutawith001@gmail.com>
2025-10-31 08:42:29 +0700

    added Open_GL test file

---
+++ test/include/main.hpp
@@ -0,0 +1,13 @@
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+# include <iostream>
+# include "glad/glad.hpp"
+# include <GLFW/glfw3.h>
+# include <thread>
+
+# ifndef PROGRAM_NAME
+#  define PROGRAM_NAME "test"
+# endif
+
+#endif
